3fc057a69c37bb6860c8340e5288155f27e52d8c
nicholashindley <nicholashindley@hotmail.com>
2024-02-22 10:32:23 +1100

    Update train_a.py

--- train_a.py
+++ train_a.py
@@ -71,7 +71,7 @@ class SupervisedDataset(Dataset):
                 'target_flow': torch.from_numpy(target_flow)}
         return data
 
-# Generate train/test split
+# generate train/test split
 im_size = 128
 batch_size = 8
 dataset = SupervisedDataset(im_dir=im_dir, im_size=im_size)
@@ -80,7 +80,7 @@ trainset, valset = torch.utils.data.dataset.random_split(dataset, split)
 trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=True)
 valloader = torch.utils.data.DataLoader(valset, batch_size=batch_size, shuffle=True)
 
-# unet
+# set up network
 vxm_model = network_a.VxmDense(im_size, int_steps=10)
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 vxm_model.to(device)
